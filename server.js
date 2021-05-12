d44cffe24fc6a873208cce5f90d109d0326ebe95
Rob Hess <hessro@oregonstate.edu>
2021-05-12 15:21:16 -0700

    Add code from 5/12 lecture.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer');
+const crypto = require('crypto');
 
 const { connectToDB } = require('./lib/mongo');
 const { getImageInfoById } = require('./models/image');
@@ -6,8 +8,35 @@ const { getImageInfoById } = require('./models/image');
 const app = express();
 const port = process.env.PORT || 8000;
 
+const acceptedFileTypes = {
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/gif': 'gif'
+};
+
+// const upload = multer({ dest: `${__dirname}/uploads` });
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: `${__dirname}/uploads`,
+    filename: (req, file, callback) => {
+      const filename = crypto.pseudoRandomBytes(16).toString('hex');
+      const extension = acceptedFileTypes[file.mimetype];
+      callback(null, `${filename}.${extension}`);
+    }
+  }),
+  fileFilter: (req, file, callback) => {
+    callback(null, !!acceptedFileTypes[file.mimetype])
+  }
+});
+
 app.get('/', (req, res, next) => {
   res.status(200).sendFile(__dirname + '/index.html');
+});
+
+app.post('/images', upload.single('image'), (req, res, next) => {
+  console.log("== req.body:", req.body);
+  console.log("== req.file:", req.file);
+  res.status(200).send();
 });
 
 app.get('/images/:id', async (req, res, next) => {
@@ -22,6 +51,13 @@ app.get('/images/:id', async (req, res, next) => {
     next(err);
   }
 });
+
+app.use('*', (err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({
+    error: "An error occurred.  Try again later."
+  })
+})
 
 app.use('*', (req, res, next) => {
   res.status(404).send({
